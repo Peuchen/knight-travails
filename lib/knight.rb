e33c2a704fa7b25d5b7400a4b8ff8a80393fa551
peuchen <npeuchen@gmail.com>
2021-01-13 12:56:12 +0100

    Set up Knight class

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,2 +1,22 @@
 class Knight
+  attr_accessor :position, :possible_moves, :children
+
+  def initialize(position)
+    @position = position
+    @possible_moves = create_move_list(position)
+    @children = []
+  end
+
+  def create_move_list(position, move_list = [])
+    moves = [[-1, -2], [-1, 2], [1, -2], [1, 2], [-2, -1], [-2, 1], [2, -1], [2, 1]]
+
+    moves.each do |move|
+      x = position[0] + move[0]
+      y = position[1] + move[1]
+
+      move_list << [x, y] if x.between?(0,7) && y.between?(0,7)
+    end
+    move_list
+  end
+
 end
